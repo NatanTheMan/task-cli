e12e18bf9b981412a40e6653dc201e63adc00fd1
Natan Costa <natanscosta7@gmail.com>
2025-11-02 12:55:48 -0300

    fix: accept Status, not string

--- src/Task.php
+++ src/Task.php
@@ -7,7 +7,7 @@ class Task
     public $description;
     public $status;
 
-    public function __construct(string $description, string $status = Status::Todo)
+    public function __construct(string $description, Status $status = Status::Todo)
     {
         $this->description = $description;
         $this->status = $status;
